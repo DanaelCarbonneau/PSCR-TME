b874ead5580ae43f8e21cd33791440faa22644b6
CyranHOEdeBergerac <danael.carbonneau@protonmail.com>
2023-09-24 10:43:01 +0200

    Tests plus propres

--- TME1/exo2/main.cpp
+++ TME1/exo2/main.cpp
@@ -2,36 +2,36 @@
 #include<iostream>
 #include "myString.hh"
 
+
 using namespace std;
 
 int main(){
+
+    /*=====Test de nos primitives sur les chaînes de caractère (char*)=====*/
+    cout << "====Tests des primitives sur les char*===="<< endl;
     const char * str1 = "lorem ipsum dolor";
-
-    cout << "Longueur de -lorem ipsum dolor- : " << pr::length(str1)<<endl;
-
+    const char* str0 = "";
+    cout << "Test de length OK : " << ((pr::length(str1) ==17) && (pr::length(str0) ==0)) <<endl;
     char* str2 = pr::newcopy(str1);
-
     cout << "Copie de la première chaîne : " << str2 << endl;
-
     delete[] str2;
 
+    /*=====Tests avec la classe mString=====*/
+    cout << "\n====Tests avec la classe mString===="<< endl;
+    /*constructeur avec une chaîne de caractères (char*)*/
+    pr::mString mstr3 = "sit amet";
+    cout << "mString : " << mstr3 << endl;   //Surcharge de <<
+    
+    /*Constructeur par copie d'un mString*/
+    pr::mString  mstr4 = mstr3;    
+    cout << "mString avec un constructeur par copie : " <<  mstr4 << endl;
 
-    pr::mString str3 = "sit amet";
+    /*Test de l'affectation (surcharge de =)*/
+    pr::mString  mstr5 = "consectetur adipiscing elit";
+     mstr4 =  mstr5;
+    cout << "mString à qui on a affecté une autre mString : " <<  mstr4 <<endl;
 
-    cout << "Voilà la chaîne de la classe string : " << str3 << endl;
-
-    pr::mString str4 ;      //On ne peut pas faire l'affectation directement (?), sinon l'opérateur re défini n'est pas utilisé...
-    str4= str3;            
-
-    cout << "Voilà la chaîne de la classe string ayant été crée en faisant une affectation : " << str4 << endl;
-
-    pr::mString str5 = "consectetur adipiscing elit";
-
-    cout << "Tests de la comparaison entre nos différentes string\n" << 
-    "\tstr3 == str4 (attendu : vrai) " << (str3==str4) <<endl
-    <<"\tstr3 == str5 (attendu : faux) " <<(str3==str5) << endl
-    <<"\tstr3 < str4 (attendu : faux) " << (str3 < str4) <<endl
-    <<"\tstr5 < str3 (attendu : vrai) " << (str5 < str3) << endl;;
-     
-
+    /*Test des comparaisons (surcharge de == et <)*/
+    bool tests = ( mstr5== mstr4) && !(mstr3== mstr5) && !(mstr3 <  mstr4) && ( mstr5 < mstr3);
+    cout << "Tests comparaison OK : " << tests << endl;
 }
