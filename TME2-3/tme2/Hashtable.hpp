5112661aabd25904f93c2860a795c453ef20b0d2
CyranHOEdeBergerac <danael.carbonneau@protonmail.com>
2023-10-06 12:42:16 +0200

    TME3 : problèmes de boucles infinies, itérateur à revoir, mais compile.

---
+++ TME2-3/tme2/Hashtable.hpp
@@ -0,0 +1,119 @@
+#include <cstddef>
+#include <vector>
+#include <forward_list>
+
+/*Boucle infinie avec les itérateurs ?
+TODO : voir pas à pas l'exécution du code pour voir pourquoi l'itérateur boucle vers l'infini*/
+
+using namespace std;
+namespace pr {
+
+template <typename K, typename V>
+
+class Hashtable{
+    public :
+    class Entry{
+        public:
+        const K key;
+        V value;
+        Entry(const K& key, const V& value):key(key), value(value){}
+    };
+    typedef std::vector <std::forward_list<Entry>> buckets_t;
+
+    class Iterator{
+        buckets_t& it_buckets ;
+        typename std::forward_list<Entry>::iterator lit;
+        size_t index;
+
+        void advance(){
+            cout << "On commence à avancer" <<endl;
+            for(; index < it_buckets.size(); index++){
+                if (!it_buckets[index].empty()){
+                    cout << "On sort" << endl;
+                    lit = it_buckets[index].begin(); //On re positionne notre itérateur de liste au début de la suivante
+                    return;
+                }
+                cout << "On est toujours sur une case vide d'index " << index << endl;
+            }
+            if (index == it_buckets.size()){
+                cout << "On est à la fin : " <<index <<endl;
+                index = -1;
+                lit = it_buckets[0].end();
+            }
+        }        
+        public :
+        Iterator(Hashtable &ht):it_buckets(ht.bucket),index(0){
+            advance();      //On va vers le début dans la hashtable
+        }
+        Iterator(Hashtable &ht, int end):it_buckets(ht.bucket){  //Constructeur d'un itérateur end
+            cout <<"Ici on fait un end" << endl;
+            index = -1;
+            lit = it_buckets[0].end();
+        }   
+        Iterator& operator++(){
+            if(++lit== it_buckets[index].end()){
+                advance();
+            }
+            return *this;
+        }
+        Entry& operator*(){
+            return *lit;
+        }
+        bool operator!=(Iterator& other){
+            return index != other.index;      //Faux mais permet de voir si le test est effectué
+        }
+    };
+
+    private :
+    buckets_t bucket ;
+    size_t sz;                    //Nombre d'entrées
+    
+    public :
+    Hashtable(size_t szb) : bucket(szb){
+        sz = 0;
+    }
+    // (Les mécanismes de la classe canonique sont implémentés par défaut par le compilateur, pas besoin de les réécrire.)
+
+    /*Fonctions pour l'itérateur*/
+
+    Iterator begin(){
+        return Iterator(*this);
+    }
+
+    Iterator end(){
+        return Iterator(*this,1);
+    }
+
+    /*Fonction qui rend l'adresse de la valeur associée à la clé key si on la trouve, nullptr sinon*/
+    V* get (const K & key){
+        size_t ind = (std::hash<K>() (key)) % bucket.size();    //Hasher standard
+
+        /*Trouver le mot dans la hashmap*/
+        for (auto& e : bucket[ind]){
+            if (e.key == key){
+                return &e.value;
+            }
+        }       
+        return nullptr;
+    }
+    /*Fonction qui ajoute l'association (key, value) à la table. 
+    * La fonction rend vrai si la valeur associée à key a été mise à jour dans la table, 
+    * et faux si on a réalisé une insertion (la clé n'était pas encore dans la table).*/
+    bool put (const K & key, const V value){
+        size_t ind = std::hash<K>() (key) % bucket.size();
+
+        for(auto & e : bucket[ind]){
+		    if (e.key == key){
+                e.value = value;
+                return true;
+            }
+    	}
+        /*On ne l'a pas trouvé dans le bucket, il faut créer l'association dans la table*/
+        bucket[ind].emplace_front(key,value);           //emplace évite de créer un temporaire
+        ++sz;                 //La taille de la Hashtable augmente
+        return false;           //On a créé une insertion
+    }
+    /*Fonction qui indique la taille de la hashtable (nombre d'entrées effectives dedans)*/
+    size_t size() const{return sz;}
+    };
+}
---
+++ TME2-3/tme2/main3.cpp
@@ -0,0 +1,134 @@
+/*Question 1. Ecrivez une fonction générique size_t count (iterator begin, iterator end) qui
+compte le nombre d'éléments entre begin et end.
+Question 2. Ecrivez une fonction générique size_t count_if_equal (iterator begin, iterator
+end, const T & val) qui compte le nombre d'éléments entre begin et end qui sont égaux à val au
+sens de ==.
+Question 3. Invoquez ces fonctions dans votre programme qui compte les mots, et contrôlez le
+résultat : par exemple une invocation*/
+
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <chrono>
+#include "Hashtable.hpp"
+using namespace std;
+
+
+
+
+/*TME3 Partie 1, Algorithm*/
+template <typename Iterator>
+size_t count (Iterator begin, Iterator end){
+    size_t cpt = 0;
+
+    for (auto it = begin; it!=end; ++it){
+        ++cpt;
+    }
+    return cpt;
+}
+
+template <typename T, typename Iterator>
+size_t count_if_equal (Iterator begin, Iterator end, const T & val){
+    size_t cpt = 0;
+
+    for (auto it = begin; it!=end; ++it){
+        if (*it == val){
+            ++cpt;
+        }
+    }
+    return cpt;
+}
+
+
+
+/*Partie 1*/
+/*Fonction qui ajoute un mot s au tableau de fréquences tab*/
+void ajouteMotVect(std::vector<std::pair<string,int>> &tab, const string& s){
+	for(size_t i = 0; i < tab.size() ; i++){
+		if( tab[i].first==s ){
+			++tab[i].second;
+			return;
+		}
+	}
+	tab.push_back(make_pair(s,1));	//On ne l'a pas trouvé dans le tableau
+}
+
+/*Fonction qui retourne le nombre d'occurences de s dans le texte dont les fréquences sont contenues dans tab*/
+int nbOccurenceMot(const std::vector<std::pair<string,int>> &tab, const string& s){
+	for (size_t i = 0; i < tab.size(); i++){
+		if (tab[i].first == s){
+			return tab[i].second;
+		}
+	}
+	return 0;
+}
+
+
+
+
+int main () {
+
+	using namespace std::chrono;
+
+	ifstream input = ifstream("WarAndPeace.txt");
+
+	auto start = steady_clock::now();
+
+	/*Partie 1*/
+	std::vector<std::pair<string,int>> tab;
+
+	/*Partie 2*/
+	pr::Hashtable<string,int> htab (30000);
+
+	cout << "Parsing War and Peace" << endl;
+
+	size_t nombre_lu = 0;
+	// prochain mot lu
+	string word;
+	// une regex qui reconnait les caractères anormaux (négation des lettres)
+	regex re( R"([^a-zA-Z])");
+	while (input >> word) {
+		// élimine la ponctuation et les caractères spéciaux
+		word = regex_replace ( word, re, "");
+		// passe en lowercase
+		transform(word.begin(),word.end(),word.begin(),::tolower);
+
+
+
+		int* word_count = htab.get(word);
+		if (word_count != nullptr){
+			++(*word_count);
+		}
+		else {
+			htab.put(word,1);		//Ajoute l'entrée à la table de hachage (il y a une occurence actuellement)
+		}
+
+		// word est maintenant "tout propre"
+		if (nombre_lu % 100 == 0)
+			// on affiche un mot "propre" sur 100
+			cout << nombre_lu << ": "<< word << endl;
+		nombre_lu++;
+	}
+	input.close();
+
+
+
+	cout << "Amount of different words " << htab.size()<<endl;
+	cout << "Occurences of \"war\" : " <<  (htab.get("war") ? *htab.get("war") : 0) << endl;
+	cout << "Occurences of \"peace\" : " << (htab.get("peace") ? *htab.get("peace") : 0) << endl;
+	cout << "Occurences of \"toto\" : " << (htab.get("toto") ? *htab.get("toto") : 0) << endl;
+
+	cout << "Finished Parsing War and Peace" << endl;
+
+    cout << "Nombre d'éléments dans la table de hachage : " << count(htab.begin(),htab.end());
+	auto end = steady_clock::now();
+    cout << "Parsing took "
+              << duration_cast<milliseconds>(end - start).count()
+              << "ms.\n";
+
+	cout << "Found a total of " << nombre_lu << " words." << endl;	
+
+    return 0;
+}
+
+
