74f5c56475a63021058839ed3abea30b3a30ed60
CyranHOEdeBergerac <danael.carbonneau@protonmail.com>
2023-11-24 12:47:16 +0100

    indications pour TME8

--- TME8/src/server.cpp
+++ TME8/src/server.cpp
@@ -2,6 +2,42 @@
 #include <iostream>
 #include <unistd.h>
 
+
+/**
+ * Indications pour faire le TP
+ * 
+ * cf : https://www.geeksforgeeks.org/socket-programming-cc/
+ * 
+ * man 7 socket(sockaddr)
+ * man 7 tcp (TCP)
+ * man 7 ip (sockaddr_in)
+ * 
+ * server 
+ * 		socket
+ * 		bind
+ * 		listen
+ * 		sock = accept
+ * 		while( ) {
+ * 		read (sock)
+ * 		decode (liste ou up ou down ou exit)
+ * 		decode arg
+ * 		execute operation
+ * }
+ * 
+ * Pour transférer les fichiers, il faut faire une fonction de copie block par block du fichier
+ * 
+ * 
+ */
+
+void copy( int fdsrc, int fddst){
+	char buff [256];			//À VOIR QUELLE TAILLE DE BLOCK
+	while (1){		// TODO : condition du while
+		read(fdsrc,buff, 256);
+		write(fddst,buff,256);
+	}
+	
+}
+
 int main00() {
 	pr::ServerSocket ss(1664);
 
